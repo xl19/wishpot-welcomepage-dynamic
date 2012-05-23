bd0da30005f1f629425a1b33e9eafb1b18efd61f
Tom Lianza <tom@lianza.org>
2012-05-23 15:42:11 -0700

    [Fixes #29854473] Run the correct errored images script after we load a pop-up store onto the page.

--- public/scripts.js
+++ public/scripts.js
@@ -102,6 +102,7 @@ function replaceListNode(data, textStatus, jqXHR)
 	jQuery(this).replaceWith(resultDocument);
 	initFluidLists();
 	resizeFacebook();
+	correctErroredImages();
 }
 
 function handleAjaxError(data, textStatus, jqXHR)
